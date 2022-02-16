a4b6d9007d247327ba2d9da46181bc955d61301a
David Rothfarb <dave_rothfarb@dfci.harvard.edu>
2022-02-16 12:21:46 -0500

    Renaming theme in functions file

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@ function nirvana_child_enqueue_styles() {
 
   $parent_style = 'nirvanas';
   wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css');
-  wp_enqueue_style('nirvana_child',
+  wp_enqueue_style('nirvana_regenerate',
     get_stylesheet_directory_uri() . '/css/style.css',
     array($parent_style), 
     wp_get_theme()->get('Version')
